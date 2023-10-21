c6fad9e70e32a91cf462b620dc1975eeb5c80307
sarthakbhatt1407 <sarthakbhatt1407@gmail.com>
2023-10-21 14:35:50 +0530

    vercel 2

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-// index.js
 const express = require("express");
-
 const app = express();
-const PORT = 4000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/userRoute");
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
 });
 
+app.use("/user", userRoute);
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.send("Hey this is my API running 22 🥳");
 });
 
 app.get("/about", (req, res) => {
   res.send("This is my about route..... ");
 });
-
-// Export the Express API
-module.exports = app;
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log("Connection Failed!");
+  });
